c56f124f82f0e3b3d32e7227dd0e86580f67f57b
Burhan Elaldi <burhanelaldi97@gmail.com>
2023-11-04 23:28:52 +0100

    Added two JavaScript problem Markdown files

---
+++ traffic-light-2.js
@@ -0,0 +1,48 @@
+"use strict";
+/**
+ * The `possibleStates` property define the states (in this case: colours)
+ * in which the traffic light can be.
+ * The `stateIndex` property indicates which of the possible states is current.
+ */
+const trafficLight = {
+  possibleStates: ["green", "orange", "red"],
+  stateIndex: 0,
+};
+
+let cycle = 0;
+while (cycle < 2) {
+  const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
+  console.log("The traffic light is on", currentState);
+
+  // TODO
+  // if the color is green, turn it orange
+  // if the color is orange, turn it red
+  // if the color is red, add 1 to cycles and turn it green
+  
+  // Change the state based on the current color
+  if (currentState === "green") {
+    trafficLight.stateIndex = 1; // Green to Orange
+  } else if (currentState === "orange") {
+    trafficLight.stateIndex = 2; // Orange to Red
+  } else {
+    trafficLight.stateIndex = 0; // Red to Green
+    cycle++; // Increment cycle count when turning from Red to Green
+  }
+}
+
+        
+  }
+  
+}
+
+/**
+ * The output should be:
+
+The traffic light is on green
+The traffic light is on orange
+The traffic light is on red
+The traffic light is on green
+The traffic light is on orange
+The traffic light is on red
+
+*/
